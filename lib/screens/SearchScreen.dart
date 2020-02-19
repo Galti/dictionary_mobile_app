a28e20b19c0afcf02def44ef50861e5efd472466
Galti <galstyantigran97@gmail.com>
2020-02-19 15:06:30 +0400

    Add SearchScreen UI

--- lib/screens/SearchScreen.dart
+++ lib/screens/SearchScreen.dart
@@ -1,12 +1,45 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
+  void onWordTap(String id) {
+    print(id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Search Screen'),
-      ),
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(5),
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: 'Enter a search term',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                )),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                child: Container(
+                  child: Text(
+                    'Text $index',
+                  ),
+                  padding: EdgeInsets.all(10),
+                ),
+                onTap: () => this.onWordTap(index.toString()),
+                enableFeedback: false,
+              );
+            },
+            // separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemCount: 30,
+          ),
+        )
+      ],
     );
   }
 }
