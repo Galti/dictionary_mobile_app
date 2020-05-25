eae15394f95aad7e23754b1f6e398ed6705a1bb0
Galti <galstyantigran97@gmail.com>
2020-05-25 10:18:30 +0400

    Add SettingsScreen UI

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -1,12 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Settings Screen'),
-      ),
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.feedback),
+          title: Text('Word Not Found?'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.info_outline),
+          title: Text('About'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.close),
+          title: Text('Exit'),
+          onTap: () {},
+        ),
+      ],
     );
   }
 }
